18870ef14595bbf8cb40aa9131ae1af4ead90a55
SKY <SkyKim1016@gmail.com>
2020-02-09 01:04:11 +0900

    Implemented timetamp and payment condition

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ const Tag = require('../models/tag');
 const Log = require('../models/log');
 const router = new express.Router();
 const chalk = require('chalk');
+const moment = require('moment')
+
 
 // router.get('/', (req,res) => res.render('login',{
 
@@ -23,13 +25,45 @@ router.get('/list', async(req,res) => {
     // const _id = req.params.id
 
     try{
-        const logObject = await Log.find({}).sort({timestamp: -1})
+        const logObject = await Log.find({}).sort({timestamp: -1}).limit(10)
 
-        // logDatas = JSON.stringify(logDocument)
-        // console.log(chalk.blueBright(guestBookObjectDatas))
+        for(index in logObject){
+           
+            logObject[index].timestampFormat = moment(logObject[index].timestamp).format('YYYY.MM.DD HH:MM')
+            if(logObject[index].payment.type === 'cash') logObject[index].payment.type = '현금' 
+
+            // console.log(chalk.bgRedBright( logObject[index].timestampFormat ))
+            // console.log(chalk.greenBright( logObject[index].type ))
+
+        }
+
+        // console.log(chalk.blueBright(logObject))
 
         res.render('list', {
             logObject : logObject
+    
+        })
+    
+    }catch(e){
+        res.status(400)
+    }
+    
+})
+
+router.get('/list/:', async(req,res) => {
+    // const _id = req.params.id
+
+    try{
+        const logObject = await Log.find({}).sort({timestamp: -1}).limit(10)
+
+
+        // logDatas = JSON.stringify(logDocument)
+        console.log(chalk.blueBright(logObject))
+
+        res.render('list', {
+            logObject : logObject
+        
+
         })
     
     }catch(e){
