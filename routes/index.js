4438874503b56c3dd6b6cb6876ef28c3e475b45c
SKY <SkyKim1016@gmail.com>
2020-02-09 15:41:09 +0900

    Implemented sum of amountCount

--- routes/index.js
+++ routes/index.js
@@ -22,26 +22,48 @@ router.get('/', (req,res) => res.render('landing',{
 }))
 
 router.get('/list', async(req,res) => {
-    // const _id = req.params.id
 
+    let totalAmount=0 , cashAmount=0 , cardAmount=0
+    let DocumentCount=0
+
+    // const _id = req.params.id
     try{
-        const logObject = await Log.find({}).sort({timestamp: -1}).limit(10)
+        
+        let logObject = await Log.find({}).sort({timestamp: -1}).limit(10)
+        documentCount = await Log.find({}).count()
 
         for(index in logObject){
            
-            logObject[index].timestampFormat = moment(logObject[index].timestamp).format('YYYY.MM.DD HH:MM')
-            if(logObject[index].payment.type === 'cash') logObject[index].payment.type = '현금' 
+            logObject[index].timestampFormat = moment(logObject[index].timestamp).format('YYYY.MM.DD HH:MM') 
+
+            totalAmount += logObject[index].amount
+
+            // console.log(chalk.blueBright( 'TotalAmount : ' + totalAmount + ' / logObjectAmount '+ logObject[index].amount))
+
+            if(logObject[index].payment.type === 'cash'){
+                logObject[index].payment.type = '현금';
+                cashAmount+= logObject[index].amount
+            }else if(logObject[index].payment.type === 'card'){
+                logObject[index].payment.type = '카드'
+                cardAmount+= logObject[index].amount
+
+            }
 
             // console.log(chalk.bgRedBright( logObject[index].timestampFormat ))
             // console.log(chalk.greenBright( logObject[index].type ))
 
+            
         }
 
-        // console.log(chalk.blueBright(logObject))
+        console.log(chalk.redBright(DocumentCount))
+
+        console.log(chalk.blueBright(logObject))
 
         res.render('list', {
+            documentCount,
+            totalAmount,
+            cashAmount,
             logObject : logObject
-    
         })
     
     }catch(e){
