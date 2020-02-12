ba3853ad72e30b5689a68bba57f89aa0229586ed
SKY <SkyKim1016@gmail.com>
2020-02-12 23:28:14 +0900

    Updated cumulative calculation

--- routes/index.js
+++ routes/index.js
@@ -28,35 +28,45 @@ router.get('/list', async(req,res) => {
 
     let dateNow = moment(Date.now()).format('YYYY.MM.DD HH:MM')
 
-    // const _id = req.params.id
+    let pageLimit = parseInt(req.query.pageLimit) || 10;
+
     try{
         
-        let logObject = await Log.find({}).sort({timestamp: -1}).limit(10)
+        //This is counting of record
         documentCount = await Log.find({}).countDocuments()
 
+        // This is for sum calculating of total amount and cash and creditCard
+        let totalAmountObject = await Log.find({}).sort({timestamp: -1})
+        for(index in totalAmountObject){
+            totalAmount += totalAmountObject[index].amount
+    
+                if(totalAmountObject[index].payment.type === 'cash' ){
+                    cashAmount+= totalAmountObject[index].amount
+                } 
+                if(totalAmountObject[index].payment.type === 'creditCard'){
+                    cardAmount+= totalAmountObject[index].amount
+                }
+        }
+
+         // This is getting page datas 
+        let logObject = await Log.find({}).sort({timestamp: -1}).limit(pageLimit)
         for(index in logObject){
-           
+
             logObject[index].timestampFormat = moment(logObject[index].timestamp).format('YYYY/MM/DD HH:MM:SS') 
-
-            totalAmount += logObject[index].amount
-
-
-            if(logObject[index].payment.type === 'cash'){
-                logObject[index].payment.type = '현금';
-                cashAmount+= logObject[index].amount
-
-            } 
-            if(logObject[index].payment.type === 'creditCard'){
-                logObject[index].payment.type = '신용카드'
-                cardAmount+= logObject[index].amount
-            }
-            if(logObject[index].payment.type === 'coupon'){
-                logObject[index].payment.type = '쿠폰'
-            }
-            if(logObject[index].payment.type === 'admin'){
-                logObject[index].payment.type = '관리자'
-            }
-            
+    
+                if(logObject[index].payment.type === 'cash' ){
+                    logObject[index].payment.type = '현금';
+                } 
+                if(logObject[index].payment.type === 'creditCard'){
+                    logObject[index].payment.type = '신용카드'
+                }
+                if(logObject[index].payment.type === 'coupon'){
+                    logObject[index].payment.type = '쿠폰'
+                }
+                if(logObject[index].payment.type === 'admin'){
+                    logObject[index].payment.type = '관리자'
+                } 
+   
         }
 
         res.render('list', {
@@ -65,11 +75,16 @@ router.get('/list', async(req,res) => {
             totalAmount,
             cashAmount,
             cardAmount,
-            logObject : logObject
+            logObject : logObject,
+            pageLimit
         })
+
+        // res.json({
+        //     logObject
+        // })
     
     }catch(e){
-        res.status(400)
+        console.log(e)
     }
     
 })
@@ -86,8 +101,6 @@ router.get('/list/:dateCondition', async(req,res) => {
 
         res.render('list.ejs', {
             logObject : logObject
-        
-
         })
     
     }catch(e){
