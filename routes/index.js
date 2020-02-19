6c08374e9df4968e490146e306fd81884fb466bc
SKY <SkyKim1016@gmail.com>
2020-02-19 13:27:24 +0900

    Updated kind of payment button

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.get('/login', (req,res) => {
     let reqSession = req.session;
     
     if(reqSession.loginCheck === 'true'){
-        res.redirect('/list')
+        res.redirect('/saleList')
     }else{
         res.render('login',{})
     }
@@ -126,7 +126,7 @@ router.get('/api/logout', function(req, res){
 
 
 
-router.get('/list', auth, async(req,res) => {
+router.get('/saleList', auth, async(req,res) => {
 
     let reqSessionName = req.session.name
 
@@ -225,7 +225,7 @@ router.get('/list', auth, async(req,res) => {
         // console.log(chalk.greenBright('queryEndDate : '+ queryEndDate))
 
          //@ This is rendering that variables into view page 
-        res.render('list', {
+        res.render('saleList', {
             reqSessionName,
             reqStartDateCalendar,
             reqEndDateCalendar ,
@@ -242,28 +242,253 @@ router.get('/list', auth, async(req,res) => {
         // })
     
     }catch(e){
-        //console.log(e)
-        res.render('error')
+        console.log(e)
+        // res.render('error')
     }
     
 })
 
-router.get('/list/:dateCondition', async(req,res) => {
-    // const _id = req.params.id
+router.get('/saleListDaily', auth, async(req,res) => {
+
+    let reqSessionName = req.session.name
+
+    let totalAmount=0 , cashAmount=0 , cardAmount=0
+    let documentCount=0 , tempRowNumber=0
+
+    let pageLimit = parseInt(req.query.pageLimit) || 10;
+
+    let reqStartDateCalendar = req.query.startDateCalendar;
+    let reqEndDateCalendar = req.query.endDateCalendar
+
+    let dateNow = moment(Date.now()).format('MM/DD/YYYY')
+
+    let queryStartDate, queryEndDate;
+
+    // let match={}
+    // if(req.query.requestDate){
+    //     match.timestamp = req.query.requestDate === 'true'
+    // }
 
     try{
-        const logObject = await Log.find({}).sort({timestamp: -1}).limit(10)
+        
+        //@ [1] This is date setting which recieved calendar date from view page 
+        //This is counting of record
+        if(reqStartDateCalendar === '' || typeof reqStartDateCalendar === 'undefined' ){
+            queryStartDate = '01/01/2020'
+        }else{
+            queryStartDate = moment(reqStartDateCalendar).utcOffset('+0700')
+            queryStartDate = queryStartDate
+        }
+    
+        if(reqEndDateCalendar === '' || typeof reqEndDateCalendar === 'undefined' ){
+            queryEndDate = '12/12/2020'
+        }else{
+            queryEndDate = moment(reqEndDateCalendar).utcOffset('+0700')
+            queryEndDate = queryEndDate.add(1,'day')
+        }
 
 
-        // logDatas = JSON.stringify(logDocument)
-        console.log(chalk.blueBright(logObject))
+        //@ [2] First Query 
+        //This is counting of record
+        documentCount = await Log.find({ timestamp : {$gte : queryStartDate ,  $lte : queryEndDate } }).countDocuments()
 
-        res.render('list.ejs', {
-            logObject : logObject
+        
+        //@ [3] Second Query
+        // This is for cumulative calculating of total amount and cash and creditCard
+        //let totalAmountObject = await Log.find({}).sort({timestamp: -1})
+        let totalAmountObject = await Log.find({ timestamp : {$gte : queryStartDate ,  $lte : queryEndDate }  }).sort({timestamp: -1})  
+        for(index in totalAmountObject){
+            totalAmount += totalAmountObject[index].amount
+    
+                if(totalAmountObject[index].payment.type === 'cash' ){
+                    cashAmount += totalAmountObject[index].amount
+                } 
+                if(totalAmountObject[index].payment.type === 'creditCard'){
+                    cardAmount += totalAmountObject[index].amount
+                }
+        }
+
+
+         //@ [4] Third Query
+        // This is getting page datas 
+        let logObject = await Log.find({ timestamp : {$gte : queryStartDate ,  $lte : queryEndDate } }).sort({timestamp: -1}).limit(pageLimit)
+        for(index in logObject){
+
+            //if(logObject[index].rowNumber == undefined || logObject[index].rowNumber === null || logObject[index].rowNumber == '' )
+            if(tempRowNumber == 0 ){
+                tempRowNumber = documentCount
+            }
+            logObject[index].rowNumber = tempRowNumber
+            tempRowNumber =  tempRowNumber -1
+            
+
+
+            logObject[index].timestampFormat = moment(logObject[index].timestamp).format('YYYY/MM/DD hh:mm:ss') 
+
+                if(logObject[index].payment.type === undefined || logObject[index].payment.type === null || logObject[index].payment.type === ''  ){
+                    logObject[index].payment.type = '없음';
+                } 
+                if(logObject[index].payment.type === 'cash' ){
+                    logObject[index].payment.type = '현금';
+                } 
+                if(logObject[index].payment.type === 'creditCard'){
+                    logObject[index].payment.type = '신용카드'
+                }
+                if(logObject[index].payment.type === 'coupon'){
+                    logObject[index].payment.type = '쿠폰'
+                }
+                if(logObject[index].payment.type === 'admin'){
+                    logObject[index].payment.type = '관리자'
+                } 
+   
+        }
+
+        // console.log(chalk.greenBright('queryStartDate : '+ queryStartDate))
+        // console.log(chalk.greenBright('queryEndDate : '+ queryEndDate))
+
+         //@ This is rendering that variables into view page 
+        res.render('saleListDaily', {
+            reqSessionName,
+            reqStartDateCalendar,
+            reqEndDateCalendar ,
+            documentCount,
+            totalAmount,
+            cashAmount,
+            cardAmount,
+            logObject : logObject,
+            pageLimit
         })
+
+        // res.json({
+        //     logObject
+        // })
     
     }catch(e){
-        res.status(400)
+        console.log(e)
+        // res.render('error')
+    }
+    
+})
+
+
+router.get('/saleListMonthly', auth, async(req,res) => {
+
+    let reqSessionName = req.session.name
+
+    let totalAmount=0 , cashAmount=0 , cardAmount=0
+    let documentCount=0 , tempRowNumber=0
+
+    let pageLimit = parseInt(req.query.pageLimit) || 10;
+
+    let reqStartDateCalendar = req.query.startDateCalendar;
+    let reqEndDateCalendar = req.query.endDateCalendar
+
+    let dateNow = moment(Date.now()).format('MM/DD/YYYY')
+
+    let queryStartDate, queryEndDate;
+
+    // let match={}
+    // if(req.query.requestDate){
+    //     match.timestamp = req.query.requestDate === 'true'
+    // }
+
+    try{
+        
+        //@ [1] This is date setting which recieved calendar date from view page 
+        //This is counting of record
+        if(reqStartDateCalendar === '' || typeof reqStartDateCalendar === 'undefined' ){
+            queryStartDate = '01/01/2020'
+        }else{
+            queryStartDate = moment(reqStartDateCalendar).utcOffset('+0700')
+            queryStartDate = queryStartDate
+        }
+    
+        if(reqEndDateCalendar === '' || typeof reqEndDateCalendar === 'undefined' ){
+            queryEndDate = '12/12/2020'
+        }else{
+            queryEndDate = moment(reqEndDateCalendar).utcOffset('+0700')
+            queryEndDate = queryEndDate.add(1,'day')
+        }
+
+
+        //@ [2] First Query 
+        //This is counting of record
+        documentCount = await Log.find({ timestamp : {$gte : queryStartDate ,  $lte : queryEndDate } }).countDocuments()
+
+        
+        //@ [3] Second Query
+        // This is for cumulative calculating of total amount and cash and creditCard
+        //let totalAmountObject = await Log.find({}).sort({timestamp: -1})
+        let totalAmountObject = await Log.find({ timestamp : {$gte : queryStartDate ,  $lte : queryEndDate }  }).sort({timestamp: -1})  
+        for(index in totalAmountObject){
+            totalAmount += totalAmountObject[index].amount
+    
+                if(totalAmountObject[index].payment.type === 'cash' ){
+                    cashAmount += totalAmountObject[index].amount
+                } 
+                if(totalAmountObject[index].payment.type === 'creditCard'){
+                    cardAmount += totalAmountObject[index].amount
+                }
+        }
+
+
+         //@ [4] Third Query
+        // This is getting page datas 
+        let logObject = await Log.find({ timestamp : {$gte : queryStartDate ,  $lte : queryEndDate } }).sort({timestamp: -1}).limit(pageLimit)
+        for(index in logObject){
+
+            //if(logObject[index].rowNumber == undefined || logObject[index].rowNumber === null || logObject[index].rowNumber == '' )
+            if(tempRowNumber == 0 ){
+                tempRowNumber = documentCount
+            }
+            logObject[index].rowNumber = tempRowNumber
+            tempRowNumber =  tempRowNumber -1
+            
+
+
+            logObject[index].timestampFormat = moment(logObject[index].timestamp).format('YYYY/MM/DD hh:mm:ss') 
+
+                if(logObject[index].payment.type === undefined || logObject[index].payment.type === null || logObject[index].payment.type === ''  ){
+                    logObject[index].payment.type = '없음';
+                } 
+                if(logObject[index].payment.type === 'cash' ){
+                    logObject[index].payment.type = '현금';
+                } 
+                if(logObject[index].payment.type === 'creditCard'){
+                    logObject[index].payment.type = '신용카드'
+                }
+                if(logObject[index].payment.type === 'coupon'){
+                    logObject[index].payment.type = '쿠폰'
+                }
+                if(logObject[index].payment.type === 'admin'){
+                    logObject[index].payment.type = '관리자'
+                } 
+   
+        }
+
+        // console.log(chalk.greenBright('queryStartDate : '+ queryStartDate))
+        // console.log(chalk.greenBright('queryEndDate : '+ queryEndDate))
+
+         //@ This is rendering that variables into view page 
+        res.render('saleListMonthly', {
+            reqSessionName,
+            reqStartDateCalendar,
+            reqEndDateCalendar ,
+            documentCount,
+            totalAmount,
+            cashAmount,
+            cardAmount,
+            logObject : logObject,
+            pageLimit
+        })
+
+        // res.json({
+        //     logObject
+        // })
+    
+    }catch(e){
+        console.log(e)
+        // res.render('error')
     }
     
 })
